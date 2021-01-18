bffc70c72ba2a90d747a9ab2db4d107adc4a1a12
Maria Rushkova <maryrushkova@gmail.com>
2021-01-19 00:04:44 +0100

    Remove unnecessary comment

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
+  interestRate: 1.2,
   pin: 1111,
 
   movementsDates: [
